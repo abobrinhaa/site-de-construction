53a790a517079660aaba76fd58c9e489729ab05c
aniger jr <98634046+abobrinhaa@users.noreply.github.com>
2024-10-10 15:42:39 -0300

    implementaoçao php retorno a pagina index

--- site acca obra/contato.php
+++ site acca obra/contato.php
@@ -2,14 +2,17 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+// Carrega o autoloader do PHPMailer
 require 'PHPMailer-master/src/Exception.php';
 require 'PHPMailer-master/src/PHPMailer.php';
 require 'PHPMailer-master/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $mensagem = $_POST['mensagem'];
+    $destinatario = "seuemail@gmail.com"; // E-mail que receberá as mensagens
+    $assunto = "Mensagem do site"; // Assunto padrão para os e-mails
+    $mensagem = isset($_POST["mensagem"]) ? $_POST["mensagem"] : "";
+    $email = isset($_POST["email"]) ? $_POST["email"] : "";
+    $nome = isset($_POST["nome"]) ? $_POST["nome"] : "";
 
     $mail = new PHPMailer(true);
 
@@ -18,26 +21,28 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com'; // Servidor SMTP do Gmail
         $mail->SMTPAuth = true;
-        $mail->Username = 'acca.construcao@gmail.com'; // Seu e-mail do Gmail
-        $mail->Password = 'acca130487'; // Senha do seu e-mail
+        $mail->Username = 'seuemail@gmail.com'; // Seu e-mail do Gmail
+        $mail->Password = 'SENHA_DE_APLICATIVO'; // Senha do App gerada
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         // Remetente e Destinatário
         $mail->setFrom($email, $nome);
-        $mail->addAddress('acca.construcao@gmail.com'); // E-mail de destino
+        $mail->addAddress($destinatario);
 
         // Conteúdo do E-mail
         $mail->isHTML(true);
-        $mail->Subject = 'Mensagem do site';
+        $mail->Subject = $assunto;
         $mail->Body = "<h3>Mensagem de: $nome</h3><p>$mensagem</p><p><strong>Contato:</strong> $email</p>";
 
+        // Enviar e-mail
         $mail->send();
-        echo 'E-mail enviado com sucesso.';
+        // Exibe a mensagem de sucesso e redireciona para o index
+        echo "<p id='success-message' class='success-message'>E-mail enviado com sucesso.</p>";
+        header('Location: index.html'); // Redireciona para a página principal (index.html)
+        exit(); // Garante que o script seja finalizado
     } catch (Exception $e) {
         echo "Erro ao enviar o e-mail: {$mail->ErrorInfo}";
     }
 }
-$mail->SMTPDebug = 2; // ou 3 para depuração completa
-
 ?>
